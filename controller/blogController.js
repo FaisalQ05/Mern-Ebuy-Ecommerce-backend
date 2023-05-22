ee963735c78f711d45829133fbbd3a74e06ed526
faisalqureshi05 <faisal199605@gmail.com>
2023-05-22 23:54:25 +0500

    blog controller

--- controller/blogController.js
+++ controller/blogController.js
@@ -16,7 +16,7 @@ const createBlog = async (req, res) => {
 
 const updateBlog = async (req, res) => {
   const { id } = req.params
-  if (!id) throw new Error("Blog is required")
+  if (!id) throw new Error("Blog id is required")
   validateMongoDbId(id)
   const updateBlog = await Blog.findOneAndUpdate({ _id: id }, req.body, {
     new: true,
@@ -27,4 +27,146 @@ const updateBlog = async (req, res) => {
   res.json(updateBlog)
 }
 
-module.exports = { createBlog, updateBlog }
+const getBlog = async (req, res) => {
+  const { id } = req.params
+  validateMongoDbId(id)
+  const blog = await Blog.findById(id).exec()
+  if (!blog) throw new Error("No Blog found")
+  blog.numViews = blog.numViews + 1
+  const updatedBlog = await blog.save()
+
+  // const updateViews = await Blog.findByIdAndUpdate(
+  //   id,
+  //   {
+  //     $inc: { numViews: 1 },
+  //   },
+  //   { new: true }
+  // )
+  res.json(updatedBlog)
+}
+
+const getAllBlog = async (req, res) => {
+  const blogs = await Blog.find().lean()
+  if (blogs.length > 0) {
+    res.json(blogs)
+  } else {
+    throw new Error("No Blog Found")
+  }
+}
+
+const deleteBlog = async (req, res) => {
+  const { id } = req.params
+  validateMongoDbId(id)
+  const deleteBlog = await Blog.findByIdAndDelete(id).lean()
+  if (!deleteBlog) {
+    throw new Error("No blog found")
+  }
+  res.json(deleteBlog)
+}
+
+const likeBlog = async (req, res) => {
+  console.log("like blog")
+  const { blogId } = req.params
+  if (!blogId) throw new Error("Blog id is required")
+  validateMongoDbId(blogId)
+  const blog = await Blog.findById(blogId)
+  const loginUserId = req?.user?._id
+  const isLiked = blog?.isLiked
+
+  const alreadyDisliked = blog?.disLikes?.find(
+    (userId) => userId?.toString() === loginUserId?.toString
+  )
+  if (alreadyDisliked) {
+    console.log("Condition 1")
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { disLikes: loginUserId },
+        isDisLiked: false,
+      },
+      { new: true }
+    )
+    res.json(blog)
+  }
+  if (isLiked) {
+    console.log("Condition 2")
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLiked: false,
+      },
+      { new: true }
+    )
+    res.json(blog)
+  } else {
+    console.log("Condition 3")
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { likes: loginUserId },
+        isLiked: true,
+      },
+      { new: true }
+    )
+    res.json(blog)
+  }
+}
+
+const dislikeBlog = async (req, res) => {
+  console.log("Dis like blog")
+  const { blogId } = req.params
+  if (!blogId) throw new Error("Blog id is required")
+  validateMongoDbId(blogId)
+  const blog = await Blog.findById(blogId)
+  const loginUserId = req?.user?._id
+  const isDisliked = blog?.isDisLiked
+
+  const alreadyDisliked = blog?.disLikes?.find(
+    (userId) => userId?.toString() === loginUserId?.toString
+  )
+  if (alreadyDisliked) {
+    console.log("Condition 1")
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { disLikes: loginUserId },
+        isDisLiked: false,
+      },
+      { new: true }
+    )
+    res.json(blog)
+  }
+  if (isLiked) {
+    console.log("Condition 2")
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLiked: false,
+      },
+      { new: true }
+    )
+    res.json(blog)
+  } else {
+    console.log("Condition 3")
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { likes: loginUserId },
+        isLiked: true,
+      },
+      { new: true }
+    )
+    res.json(blog)
+  }
+}
+
+module.exports = {
+  createBlog,
+  updateBlog,
+  getBlog,
+  getAllBlog,
+  deleteBlog,
+  likeBlog,
+}
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const { authMiddleware, isAdmin } = require("../middleware/authMiddleware")
-const { createBlog, updateBlog } = require("../controller/blogController")
+const {
+  createBlog,
+  updateBlog,
+  getBlog,
+  getAllBlog,
+  deleteBlog,
+  likeBlog,
+} = require("../controller/blogController")
 const router = express.Router()
 
 router.post("/", authMiddleware, isAdmin, createBlog)
 router.put("/:id", authMiddleware, isAdmin, updateBlog)
+router.get("/:id", getBlog)
+router.get("/", getAllBlog)
+router.delete("/:id", authMiddleware, isAdmin, deleteBlog)
+router.put("/:blogId/likes", authMiddleware, isAdmin, likeBlog)
 
 module.exports = router
